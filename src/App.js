daf6703287b82f82f17707a029463ae993ec3098
MaddyAngaran <maddyangaran@gmail.com>
2024-09-16 21:08:53 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Products from './components/Products';
+import Pricing from './components/Pricing';
+import About from './components/AboutUs';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <h1>Dolce Farina</h1>
+          <nav className="nav">
+                <Link to="/">Home</Link>
+                <Link to="/Products">Products</Link>
+                <Link to="/Pricing">Pricing</Link>
+                <Link to="/AboutUs">About Us</Link>
+                <a href="https://www.instagram.com/direct/new/?username=dolcefarina_" target="_blank" rel="noopener noreferrer">Contact</a>
+              </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/AboutUs" element={<About/>}/>
+        <Route path="/Products" element={<Products/>}/>
+        <Route path="/Pricing" element={<Pricing/>}/>
+
+      </Routes>
     </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,58 @@
+import orange from '../photos/orange.jpg';
+import blueberry from '../photos/blueberry.jpg';
+import twins from '../photos/twins.jpg';
+
+import cake1 from '../photos/cake1.jpg';
+import cake2 from '../photos/cake2.jpg';
+import cake3 from '../photos/cake3.jpg';
+import cake4 from '../photos/cake4.jpg';
+import cake5 from '../photos/cake5.jpg';
+import cake6 from '../photos/cake6.jpg';
+import cake7 from '../photos/cake7.jpg';
+import cake8 from '../photos/cake8.jpg';
+import cake9 from '../photos/cake9.jpg';
+import cake10 from '../photos/cake10.jpg';
+
+
+
+function Home(){
+    return(
+        <div className='App-header'>
+                <div class="banner">
+                    <img src={orange} alt="orange" />
+                    <img src={twins} alt="twins" />
+                    <img src={blueberry} alt="blueberry"/>
+                </div>
+            <div className="Outer-order">
+                <div className='Order'>
+                    <a href="https://www.instagram.com/direct/new/?username=dolcefarina_">Order Now</a>
+                </div>
+            </div>
+
+            <h1 className='Home-header'>Making the community sweeter one day at a time</h1>
+            <div className='Photo-album'>
+                <div class="image-border">
+                    <div class="image-container">
+                        <img src={cake1} alt="cake1" />
+                        <img src={cake2} alt="cake2" />
+                        <img src={cake3} alt="cake3"/>
+                        <img src={cake4} alt="cake4" />
+                        <img src={cake5} alt="cake5" />
+                    </div>
+                </div>
+                <div class="image-border">
+                    <div class="image-container">
+                        <img src={cake6} alt="cake6" />
+                        <img src={cake7} alt="cake7" />
+                        <img src={cake8} alt="cake8"/>
+                        <img src={cake9} alt="cake9" />
+                        <img src={cake10} alt="cake10" />
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    )
+}
+
+export default Home;
